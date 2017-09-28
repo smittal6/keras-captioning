d4b402b0abce655b2811a172584532592f1256fa
Siddharth Mittal <smittal6@gmail.com>
2017-09-28 23:04:29 +0530

    check for equality and default follow non default

--- model.py
+++ model.py
@@ -8,20 +8,20 @@ from keras.layers import Input, BatchNormalization
 from keras.layers.core import Dense, Activation
 from keras.layers.recurrent import LSTM
 
-def model_image(mtype='vgg',input_tensor):
+def model_image(input_tensor,mtype='vgg'):
     '''
     Loads the image model.
     We have three options: VGG16, Resnet, InceptionV3
     args:
         mtype: {vgg,resnet,inception}, Default: 'vgg'. Used for indicating the type of image model
     '''
-    if mtype='resnet':
+    if mtype=='resnet':
         # One FC at Top
         from keras.applications.resnet50 import ResNet50 as cnn_model
-    if mtype='inception':
+    if mtype=='inception':
         # One FC at Top
         from keras.applications.inception_v3 import InceptionV3 as cnn_model
-    if mtype='vgg':
+    if mtype=='vgg':
         # VGG has three FC layers at the top
         from keras.applications.vgg16 import VGG16 as cnn_model
 
@@ -30,6 +30,6 @@ def model_image(mtype='vgg',input_tensor):
     base_model=cnn_model(weights='imagenet',input_tensor=input_tensor,include_top=False,pooling='avg')
 
 # Just put for testing
-model_image('vgg',Input(shape=(224,224,3)))
+model_image(Input(shape=(224,224,3)),'inception')
 
 
