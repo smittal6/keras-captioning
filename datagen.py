b9d454ee4da01c3aa4b564b03b1f48290e0fdaa8
Rohit Bose <rohit.kr.bose@gmail.com>
2017-10-05 12:41:49 +0530

    Made dictionary changes

--- datagen.py
+++ datagen.py
@@ -9,10 +9,10 @@ from keras.models import Model, Sequential
 from keras.layers import Embedding
 from keras.models import load_model
 
-GLOVE_DIR = os.getcwd()+'/GLOVE/'
+GLOVE_DIR = '/GLOVE/'
 EMBEDDING_DIM = 50
 MAX_SEQUENCE_LENGTH = 50
-MAX_NB_WORDS = 20000
+VOCAB_SIZE = 20000
 PATH_TRAIN = '/Flickr8k_text/flickr_8k_train_dataset.txt'
 
 class dataFeeder():
@@ -22,14 +22,14 @@ class dataFeeder():
         Returns the embedding for the sentence.
         '''
         sequence=[item for sublist in self.tokenizer.texts_to_sequences(text.split()) for item in sublist]
-        b = np.pad(sequence, (0,EMBEDDING_DIM - len(sequence)%EMBEDDING_DIM), 'constant')
+        b = np.pad(sequence, (0,params['EMBEDDING_DIM'] - len(sequence)%params['EMBEDDING_DIM']), 'constant')
         return b
 
     def getHotVec(self,text):
         '''
         Returns the many hot vector as required by output
         '''
-        x = np.zeros(MAX_NB_WORDS)
+        x = np.zeros(params['VOCAB_SIZE'])
         words = text.split()
         for word in words:
             if (word in self.word_index):
@@ -46,7 +46,7 @@ class dataFeeder():
             encode_list = []
             p1_embed_list = []
             p2_hot_list = []
-            with open(os.getcwd()+PATH_TRAIN) as f:
+            with open(params['PATH_TRAIN']) as f:
                     lines = random.sample(f.readlines(),batch_size)
 
             for i,line in enumerate(lines):
@@ -62,9 +62,9 @@ class dataFeeder():
             inputs=[np.asarray(encode_list),np.asarray(p1_embed_list)]
             yield (inputs, np.asarray(p2_hot_list))
 
-    def __init__(self,picklefile,modelfile=None):
+    def __init__(self,params,picklefile,modelfile=None):
         texts = []
-        with open(os.getcwd()+PATH_TRAIN) as inf:
+        with open(params['PATH_TRAIN']) as inf:
             for line in inf:
                 a = line.replace("\n","")
                 texts.append(a[a.index('\t')+1:])
@@ -72,17 +72,17 @@ class dataFeeder():
 
         print texts[0]
 
-        tokenizer = Tokenizer(num_words = MAX_NB_WORDS)
+        tokenizer = Tokenizer(num_words = params['VOCAB_SIZE'])
         tokenizer.fit_on_texts(texts)
         sequences = tokenizer.texts_to_sequences(texts)
         word_index = tokenizer.word_index
-        data = pad_sequences(sequences, maxlen=MAX_SEQUENCE_LENGTH)
+        data = pad_sequences(sequences, maxlen = params['MAX_SEQUENCE_LENGTH'])
 
         self.tokenizer = tokenizer
         self.word_index = word_index
 
         embeddings_index = {}
-        f = open(os.path.join(GLOVE_DIR, 'glove.6B.50d.txt'))
+        f = open(os.path.join(params['GLOVE_DIR'], 'glove.6B.50d.txt'))
         for line in f:
             values = line.split()
             word = values[0]
@@ -93,7 +93,7 @@ class dataFeeder():
 
         # print 'Found %s word vectors.' % len(embeddings_index)
 
-        embedding_matrix = np.zeros((len(word_index) + 1, EMBEDDING_DIM))
+        embedding_matrix = np.zeros((len(word_index) + 1, params['EMBEDDING_DIM']))
         for word, i in word_index.items():
             embedding_vector = embeddings_index.get(word)
             if embedding_vector is not None:
@@ -108,7 +108,7 @@ class dataFeeder():
 
         if modelfile==None:
             self.model = Sequential()
-            embedding_layer = Embedding(len(word_index) + 1,  EMBEDDING_DIM, weights=[embedding_matrix], input_length=MAX_SEQUENCE_LENGTH, trainable=False)
+            embedding_layer = Embedding(len(word_index) + 1,  params['EMBEDDING_DIM'], weights=[embedding_matrix], input_length=params['MAX_SEQUENCE_LENGTH'], trainable=False)
             self.model.add(embedding_layer)
         else:
             self.model = load_model(modelfile);
